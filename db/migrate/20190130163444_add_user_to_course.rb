f089bb6a10f8265822e7131959993f4d94ba015a
totaotata <totaotata@protonmail.com>
2019-01-30 17:41:27 +0100

    OKKKKK

---
+++ db/migrate/20190130163444_add_user_to_course.rb
@@ -0,0 +1,5 @@
+class AddUserToCourse < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :users, :course, foreign_key: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+User.destroy_all
+Course.destroy_all
+2.times do
+  course = Course.create!(title: Faker::Simpsons.location)
+end
+10.times do
+  user = User.create!(name: Faker::Simpsons.character, course_id: Course.order("RANDOM()").first.id)
+end
